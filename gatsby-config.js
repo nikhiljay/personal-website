4dc2dee4a5b45dd981dd36b54e650ca4803c0b2b
Nikhil D'Souza <nikhiljay7@gmail.com>
2018-07-24 17:22:41 -0700

    Added website.

---
+++ gatsby-config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Nikhil D\'Souza',
+    description: 'Nikhil\'s personal website.',
+    keywords: 'tech, science, machine learning, artificial intelligence, ios development, web development, palo alto, gunn, purdue'
+  },
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-source-contentful',
+      options: {
+        spaceId: 'qd3fn9ngw366',
+        accessToken: 'b5101be5b4bbaace4b4b80448f99832c1b7c1207daa826c9b88ca37e275ebf77'
+      }
+    }
+  ],
+}
