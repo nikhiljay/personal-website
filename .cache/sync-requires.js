fe3a111f83d513193df27cd24bd1754532a9e62a
Nikhil D'Souza <nikhiljay7@gmail.com>
2018-07-29 02:35:45 -0700

    Added 2016-2017 projects + pages for all projects.

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -10,7 +10,14 @@ exports.components = {
   "component---cache-dev-404-page-js": preferDefault(require("/Users/nikhildsouza/Documents/Development/Personal-Website-React/.cache/dev-404-page.js")),
   "component---src-pages-404-js": preferDefault(require("/Users/nikhildsouza/Documents/Development/Personal-Website-React/src/pages/404.js")),
   "component---src-pages-index-js": preferDefault(require("/Users/nikhildsouza/Documents/Development/Personal-Website-React/src/pages/index.js")),
-  "component---src-pages-brainwaves-js": preferDefault(require("/Users/nikhildsouza/Documents/Development/Personal-Website-React/src/pages/Brainwaves.js"))
+  "component---src-pages-brainwaves-js": preferDefault(require("/Users/nikhildsouza/Documents/Development/Personal-Website-React/src/pages/Brainwaves.js")),
+  "component---src-pages-wolly-js": preferDefault(require("/Users/nikhildsouza/Documents/Development/Personal-Website-React/src/pages/Wolly.js")),
+  "component---src-pages-prioritize-js": preferDefault(require("/Users/nikhildsouza/Documents/Development/Personal-Website-React/src/pages/Prioritize.js")),
+  "component---src-pages-help-nepal-js": preferDefault(require("/Users/nikhildsouza/Documents/Development/Personal-Website-React/src/pages/HelpNepal.js")),
+  "component---src-pages-wwdc-scholars-js": preferDefault(require("/Users/nikhildsouza/Documents/Development/Personal-Website-React/src/pages/WWDCScholars.js")),
+  "component---src-pages-hack-chair-js": preferDefault(require("/Users/nikhildsouza/Documents/Development/Personal-Website-React/src/pages/HackChair.js")),
+  "component---src-pages-gunn-hacks-js": preferDefault(require("/Users/nikhildsouza/Documents/Development/Personal-Website-React/src/pages/GunnHacks.js")),
+  "component---src-pages-ml-projects-js": preferDefault(require("/Users/nikhildsouza/Documents/Development/Personal-Website-React/src/pages/MLProjects.js"))
 }
 
 exports.json = {
@@ -19,5 +26,12 @@ exports.json = {
   "404.json": require("/Users/nikhildsouza/Documents/Development/Personal-Website-React/.cache/json/404.json"),
   "index.json": require("/Users/nikhildsouza/Documents/Development/Personal-Website-React/.cache/json/index.json"),
   "404-html.json": require("/Users/nikhildsouza/Documents/Development/Personal-Website-React/.cache/json/404-html.json"),
-  "brainwaves.json": require("/Users/nikhildsouza/Documents/Development/Personal-Website-React/.cache/json/brainwaves.json")
+  "brainwaves.json": require("/Users/nikhildsouza/Documents/Development/Personal-Website-React/.cache/json/brainwaves.json"),
+  "wolly.json": require("/Users/nikhildsouza/Documents/Development/Personal-Website-React/.cache/json/wolly.json"),
+  "prioritize.json": require("/Users/nikhildsouza/Documents/Development/Personal-Website-React/.cache/json/prioritize.json"),
+  "help-nepal.json": require("/Users/nikhildsouza/Documents/Development/Personal-Website-React/.cache/json/help-nepal.json"),
+  "wwdc-scholars.json": require("/Users/nikhildsouza/Documents/Development/Personal-Website-React/.cache/json/wwdc-scholars.json"),
+  "hack-chair.json": require("/Users/nikhildsouza/Documents/Development/Personal-Website-React/.cache/json/hack-chair.json"),
+  "gunn-hacks.json": require("/Users/nikhildsouza/Documents/Development/Personal-Website-React/.cache/json/gunn-hacks.json"),
+  "ml-projects.json": require("/Users/nikhildsouza/Documents/Development/Personal-Website-React/.cache/json/ml-projects.json")
 }
