7ef2869cc7c5510512f45dd3fdde3742985bdf84
Nikhil D'Souza <nikhiljay7@gmail.com>
2019-04-23 20:45:28 -0400

    Added gatsby-plugin-styled-components

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,6 +12,7 @@ module.exports = {
         spaceId: 'qd3fn9ngw366',
         accessToken: 'b5101be5b4bbaace4b4b80448f99832c1b7c1207daa826c9b88ca37e275ebf77'
       }
-    }
+    },
+    'gatsby-plugin-styled-components'
   ],
 }
