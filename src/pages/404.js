d370144ba86d06613119baed44ff6b9b389c2dce
Nikhil D'Souza <nikhiljay7@gmail.com>
2019-04-29 17:42:25 -0400

    Fixed styling issues on 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,25 +1,28 @@
 import React from 'react'
 import Favicon from 'react-favicon';
+import Layout from '../components/layout'
 
 const NotFoundPage = () => (
-  <div className="container">
-    <Favicon url={require('../images/favicon.png')} />
-    <div className="code-area">
-      {/* eslint-disable-next-line */}
-      <span className="comment">// 404 page not found.</span>
-      <span>
-        <span className="if">if</span>
-        <span className="condition"> (</span>
-        <span className="blue">!</span><span className="condition">found)</span>
-        <span> &#123;</span>
-      </span>
-      <span>
-        <span className="nested blue">throw</span>
-        <span>(<span className="message">"(╯°□°)╯︵ ┻━┻"</span>);</span>
-        <span style={{display: 'block'}}>&#125;</span>
-      </span>
+  <Layout>
+    <div className="container">
+      <Favicon url={require('../images/favicon.png')} />
+      <div className="code-area">
+        {/* eslint-disable-next-line */}
+        <span className="comment">// 404 page not found.</span>
+        <span>
+          <span className="if">if</span>
+          <span className="condition"> (</span>
+          <span className="blue">!</span><span className="condition">found)</span>
+          <span> &#123;</span>
+        </span>
+        <span>
+          <span className="nested blue">throw</span>
+          <span>(<span className="message">"(╯°□°)╯︵ ┻━┻"</span>);</span>
+          <span style={{display: 'block'}}>&#125;</span>
+        </span>
+      </div>
     </div>
-  </div>
+  </Layout>
 )
 
 export default NotFoundPage
