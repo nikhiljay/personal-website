d5015b5d4d5f85d0921af89d421bcdec0e8a1ce8
Nikhil D'Souza <nikhiljay7@gmail.com>
2022-12-09 04:05:57 -0500

    Sizing changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,24 +18,24 @@ const IndexPage = () => (
       alt=""
       className="fixed -bottom-20 -right-20 select-none"
     />
-    <div className="relative mx-auto max-w-xl py-28 px-4">
+    <div className="relative mx-auto max-w-2xl py-40 px-4">
       <div className="flex justify-between items-center">
         <div className="flex items-center">
           <StaticImage
             src={"../images/profile.png"}
             alt="sup"
             placeholder="none"
-            width="80"
-            height="80"
+            width="85"
+            height="85"
             quality={100}
             draggable="false"
             imgStyle={{ borderRadius: `100%` }}
             className="select-none"
           />
           <div className="ml-4">
-            <h1 className="text-3xl font-extrabold text-white antialiased -mb-1.5">Nikhil D'Souza</h1>
+            <h1 className="text-4xl font-extrabold text-white antialiased -mb-1">Nikhil D'Souza</h1>
             <FaLocationArrow size={10} className="inline-block text-slate-400 mr-1" />
-            <p className="inline-block text-slate-400 text-xs font-medium antialiased">San Francisco, CA</p>
+            <p className="inline-block text-slate-400 text-sm font-medium antialiased">San Francisco, CA</p>
           </div>
         </div>
         <div className="flex-initial h-5">
@@ -74,16 +74,16 @@ const IndexPage = () => (
           </a>
         </div>
       </div>
-      <p className="text-slate-400 antialiased text-sm my-6">Currently Cofounder & CTO of <a href="https://vitalizecare.co" target="_blank" rel="noreferrer" className="transition ease-in-out duration-200 text-orange-400 hover:text-orange-500 font-semibold">Vitalize Care</a>, where we're building a platform to help improve the mental health of our healthcare workers. I love learning, telling stories with data, and thinking about the intersection of AI & health. In my free time, I enjoy playing chess, tennis, and the piano.</p>
+      <p className="text-slate-400 antialiased my-6">Currently Cofounder & CTO of <a href="https://vitalizecare.co" target="_blank" rel="noreferrer" className="transition ease-in-out duration-200 text-orange-400 hover:text-orange-500 font-semibold">Vitalize Care</a>, where we're building a platform to help improve the mental health of our healthcare workers. I love learning, telling stories with data, and thinking about the intersection of AI & health. In my free time, I enjoy playing chess, tennis, and the piano.</p>
       <a
         href="mailto:nikhiljay7@gmail.com"
-        className="transition ease-in-out duration-200 inline-block text-xs font-medium rounded-md bg-orange-400 px-3 mr-3 py-2 text-white shadow-sm hover:bg-orange-500"
+        className="transition ease-in-out duration-200 inline-block text-sm antialiased font-medium rounded-md bg-orange-400 px-3 mr-3 py-2 text-white shadow-sm hover:bg-orange-500"
       >
         Contact me
       </a>
       <a
         href="https://www.linkedin.com/in/nikhiljdsouza/"
-        className="transition ease-in-out duration-200 inline-block text-xs antialiased font-semibold rounded-md bg-slate-800 px-3 py-2 text-white ring-1 ring-slate-700 hover:bg-slate-700 hover:ring-slate-700"
+        className="transition ease-in-out duration-200 inline-block text-sm antialiased font-semibold rounded-md bg-slate-800 px-3 py-2 text-white ring-1 ring-slate-700 hover:bg-slate-700 hover:ring-slate-700"
         target="_blank"
         rel="noreferrer"
       >
