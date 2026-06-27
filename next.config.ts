8d058c6a026a7d2a4c6437d2de662ccd4c23c942
Nikhil D'Souza <nikhil@vitalize.care>
2026-06-26 18:29:14 -0700

    Support testing on local iphone

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  allowedDevOrigins: ["10.0.0.179", "10.0.0.179:3000"],
   images: {
     formats: ["image/avif", "image/webp"],
   },
